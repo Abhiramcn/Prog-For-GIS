35e4c117b1eacc1f901e60e93ed42aabd71409ed
Abhiramcn <abhiramneelakandan3@gmail.com>
2026-09-14 09:54:53 +0530

    Lab1

---
+++ Lab2/L2Q/L@QS.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+numbers = np.arange(1, 11)
+
+mean = np.mean(numbers)
+maximum = np.max(numbers)
+minimum = np.min(numbers)
+
+print("Array:", numbers)
+print("Mean:", mean)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
